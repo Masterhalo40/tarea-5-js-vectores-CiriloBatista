1fb875271a752f683cf2d271017609865f9f3363
1j <1j@iMac-de-iMac03.local>
2018-11-13 19:38:04 -0600

    Igual pero con avances

--- problema5-8.js
+++ problema5-8.js
@@ -10,16 +10,18 @@ var btnArray2 = document.querySelector('#btnArray2');
 btnArray2.addEventListener('click', problema6);
 var btnArray3 = document.querySelector('#btnArray3');
 btnArray3.addEventListener('click', problema7);
+var asistentes = new Array();
 
 function problema5(){
-    var number = lista.value;
-    var vector = new Array();
+    var nombre = lista.value;
 
-    vector.unshift(number);
-    for(var i = 0; i < vector.length; i++){
+    asistentes.unshift(nombre);
+    asistentes.sort();
+    result.innerHTML = '';
+    for(var i = 0; i < asistentes.length; i++){
         row = result.insertRow(-1);
         cell = row.insertCell(0);
-        cell.innerHTML = vector[i];
+        cell.innerHTML = asistentes[i];
     }
 }
 function problema6(){
@@ -48,16 +50,30 @@ function problema7(){
     var nombre = reservar1.value;
     var vector = new Array(35);
 
+    var count = false;
+    var disp = '';
+    while(count === true){
+        alert('Este asiento ya esta ocupado!');
+    }
+    result.innerHTML = '';
     for(var i = 1; i <= vector.length; i++){
+
         row = result.insertRow(-1);
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
         cell1.innerHTML = i;
-        cell2.innerHTML = 'DISPONIBLE!';
-        cell.innerHTML = '';
+
+        if(!(nombre === '') && (number === i)){
+            cell2.innerHTML = nombre;
+            count = true;
+        }else{
+            cell2.innerHTML = 'DISPONIBLE!';
+            disp = disp+1;
+        }
         if(number === i){
             vector[i] = nombre;
             cell2.innerHTML = nombre;
+            
         }
     }
     
